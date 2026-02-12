545dd385d113aae6cfead85a2bb418552aad5c43
TulsiKeswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-12 13:59:20 +0530

    Time: 1002 ms (6.55%), Space: 47.4 MB (7.68%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -3,18 +3,18 @@ class Solution {
         int n = s.length();
         int maxl= Integer.MIN_VALUE;
         for(int i = 0; i < n; i++){
-            HashSet<Character> set = new HashSet<>();
-            int arr[] = new int[26];
+            HashMap<Character,Integer> map = new HashMap<>();
+            // int arr[] = new int[26];
 
             for(int j = i; j < n; j++){
                 char ch = s.charAt(j);
-                set.add(ch);
-                arr[ch - 'a']++;
-
-                int check = arr[ch - 'a'];
+                map.put(ch, map.getOrDefault(ch,0) + 1);
+                HashSet<Integer> set = new HashSet<>();
                 boolean isBalanced = true;
-                for(char c : set){
-                    if(arr[c - 'a'] != check){
+                for(int val : map.values()){
+                    set.add(val);
+
+                    if(set.size() > 1) {
                         isBalanced = false;
                         break;
                     }
