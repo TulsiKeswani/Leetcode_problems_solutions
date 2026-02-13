15dbae18c0da4a5d589f3d7619b52d78177f5176
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-13 10:50:55 +0530

    Time: 273 ms (5.09%), Space: 47.6 MB (12.99%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProd = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            int prod = 1;
+
+            for(int j = i; j  < n; j++){
+                prod *= nums[j];
+
+                maxProd = Math.max(prod,maxProd);
+            }
+        }
+
+        return maxProd;
+    }
+}
