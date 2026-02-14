048105777ba3a04c506fce28d71d5c67cd846e00
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-14 18:46:05 +0530

    Time: 0 ms (100%), Space: 53.2 MB (7.13%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -7,12 +7,44 @@ class Solution {
 
         // return nums[0];
 
-        // solution 2 --> xor operator
-        int xor = 0;
-        for(int i = 0; i < nums.length; i++){
-            xor ^= nums[i];
+        // // solution 2 --> xor operator
+        // int xor = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     xor ^= nums[i];
+        // }
+
+        // return xor;
+
+        // Binary search
+
+        int st = 0;
+        int n = nums.length;
+        int end = n-1;
+
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+
+            if(st == end || (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])){
+                return nums[mid];
+            }
+            else if(nums[mid] == nums[mid+1]){
+               if((end - mid + 1) % 2 != 0){
+                   st = mid+2;
+               }
+               else{
+                    end = mid-1;
+               }
+            }
+            else{
+                if((mid - st + 1) % 2 != 0){
+                   end = mid-2;
+               }
+               else{
+                    st = mid+1;
+               }
+            }
         }
 
-        return xor;
+        return -1;
     }
 }
