00cca1d4599eeb534b623264e92351cf844551a7
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-14 18:23:41 +0530

    Time: 1 ms (25.88%), Space: 53 MB (35.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,18 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // solution 1 --> Brute force (linear traversal)
-        for(int i = 0; i < nums.length; i+=2){
-            if(i == nums.length-1 || nums[i] != nums[i+1]) return nums[i];
+        // // solution 1 --> Brute force (linear traversal)
+        // for(int i = 0; i < nums.length; i+=2){
+        //     if(i == nums.length-1 || nums[i] != nums[i+1]) return nums[i];
+        // }
+
+        // return nums[0];
+
+        // solution 2 --> xor operator
+        int xor = 0;
+        for(int i = 0; i < nums.length; i++){
+            xor ^= nums[i];
         }
 
-        return nums[0];
+        return xor;
     }
 }
