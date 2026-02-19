4abac5f5c6f95a5c3b75c7e200932c917898f3da
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-19 13:49:32 +0530

    Time: 1 ms (98.46%), Space: 47.7 MB (7.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,37 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-       for(int i = 0; i < nums.length; i++){
-            int lsum = 0;
-            int rsum = 0;
-            for(int j = 0; j < i; j++){
-                lsum+=nums[j];
-            }
-            for(int j = i+1; j < nums.length; j++){
-                rsum+=nums[j];
-            }
+        // brute force
+    //    for(int i = 0; i < nums.length; i++){
+    //         int lsum = 0;
+    //         int rsum = 0;
+    //         for(int j = 0; j < i; j++){
+    //             lsum+=nums[j];
+    //         }
+    //         for(int j = i+1; j < nums.length; j++){
+    //             rsum+=nums[j];
+    //         }
 
-            if(lsum == rsum) return i;
-       }
+    //         if(lsum == rsum) return i;
+    //    }
 
-       return -1;
+    //    return -1;
+
+        // better 
+        int n = nums.length;
+        int leftsum[] = new int[n];
+        int rightsum[] = new int[n];
+        leftsum[0] = 0;
+        rightsum[n-1] = 0;
+
+        for(int i = 1; i < n; i++){
+            leftsum[i] = leftsum[i-1] + nums[i - 1];
+            rightsum[n-i-1] = rightsum[n-i] + nums[n-i];
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(leftsum[i] == rightsum[i]) return i;
+        }
+        
+        return -1;
     }
 }
