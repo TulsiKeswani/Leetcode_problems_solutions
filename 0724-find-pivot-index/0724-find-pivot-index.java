9587937195b370651bcb3e0a7af938ae41c7c0cc
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-19 13:55:00 +0530

    Time: 1 ms (98.46%), Space: 47.6 MB (39.11%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -17,21 +17,39 @@ class Solution {
     //    return -1;
 
         // better 
-        int n = nums.length;
-        int leftsum[] = new int[n];
-        int rightsum[] = new int[n];
-        leftsum[0] = 0;
-        rightsum[n-1] = 0;
+        // int n = nums.length;
+        // int leftsum[] = new int[n];
+        // int rightsum[] = new int[n];
+        // leftsum[0] = 0;
+        // rightsum[n-1] = 0;
 
-        for(int i = 1; i < n; i++){
-            leftsum[i] = leftsum[i-1] + nums[i - 1];
-            rightsum[n-i-1] = rightsum[n-i] + nums[n-i];
-        }
+        // for(int i = 1; i < n; i++){
+        //     leftsum[i] = leftsum[i-1] + nums[i - 1];
+        //     rightsum[n-i-1] = rightsum[n-i] + nums[n-i];
+        // }
         
-        for(int i = 0; i < n; i++){
-            if(leftsum[i] == rightsum[i]) return i;
+        // for(int i = 0; i < n; i++){
+        //     if(leftsum[i] == rightsum[i]) return i;
+        // }
+
+        // return -1;
+
+        // optimal
+
+        int n = nums.length-1;
+        int total = 0;
+
+        for(int i = 0; i <= n; i++){
+            total+=nums[i];
         }
-        
+
+        int lsum = 0;
+        for(int i = 0; i <= n; i++){
+            
+            if(lsum == (total - lsum - nums[i])) return i;
+            lsum+=nums[i];
+        }
+
         return -1;
     }
 }
