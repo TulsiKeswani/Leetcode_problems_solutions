db3b0ddf8c18846ce3afb70df2afdad557a91c83
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-19 10:59:45 +0530

    Time: 3 ms (88.37%), Space: 48.4 MB (42.87%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -19,11 +19,32 @@ class Solution {
 
         return false;
     }
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] matrix, int t) {
         // better --> applying binary search
 
-        for(int i = 0; i < matrix.length; i++){
-            if(binarySearch(matrix[i],target)) return true;
+        // for(int i = 0; i < matrix.length; i++){
+        //     if(binarySearch(matrix[i],t)) return true;
+        // }
+
+        // return false;
+
+        // best
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int i = n-1;
+        int j = 0;
+
+        while(i >= 0 && j <= m-1){
+            int val = matrix[i][j];
+            if(val == t){
+                return true;
+            }
+            else if(val < t){
+                j++;
+            }
+            else{
+                i--;
+            }
         }
 
         return false;
