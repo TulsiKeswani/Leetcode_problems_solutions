2194f54ce46d34c46827c87fb36e7b6ca169ca45
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-20 23:05:21 +0530

    Time: 11 ms (7.42%), Space: 55.7 MB (6.16%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,23 +1,45 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxLen = 0;
-       for(int i = 0; i < nums.length; i++){
-            int count = 0;
+    //     int maxLen = 0;
+    //    for(int i = 0; i < nums.length; i++){
+    //         int count = 0;
 
-            for(int j = i; j < nums.length; j++){
-                if(nums[j] == 1 || count < k){
-                    maxLen = Math.max(maxLen,j - i + 1);
-                }
-                else{
-                    break;
-                }
+    //         for(int j = i; j < nums.length; j++){
+    //             if(nums[j] == 1 || count < k){
+    //                 maxLen = Math.max(maxLen,j - i + 1);
+    //             }
+    //             else{
+    //                 break;
+    //             }
 
-                if(nums[j] == 0){
-                    count++;
-                }
+    //             if(nums[j] == 0){
+    //                 count++;
+    //             }
+    //         }
+    //    }
+
+    //    return maxLen;
+
+        int i = 0;
+        int j = 0;
+        int maxL = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        while(j < nums.length){
+            if(nums[j] == 0){
+                q.add(j);
             }
-       }
 
-       return maxLen;
+            if(nums[j] == 1 || q.size() <= k){
+                maxL = Math.max(maxL,j - i + 1);
+            }
+            else{
+                i = q.remove() + 1;
+            }
+
+            j++;
+        }
+
+        return maxL;
     }
 }
