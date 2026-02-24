c5e1120bf165848817274f34a8dc77715552235b
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-24 22:57:42 +0530

    Time: 1 ms (94.98%), Space: 85.4 MB (19.65%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,17 +1,34 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return true;
+        // Approach 1 --> O(n);
 
-        if(nums[0] <= nums[n-1]){
-            for(int i = 0; i<nums.length-1; i++){
-                if(nums[i] > nums[i+1]) return false;
+        // int n = nums.length;
+        // if(n == 1) return true;
+
+        // if(nums[0] <= nums[n-1]){
+        //     for(int i = 0; i<nums.length-1; i++){
+        //         if(nums[i] > nums[i+1]) return false;
+        //     }
+        // }
+        // else{
+        //     for(int i = 0; i< nums.length-1; i++){
+        //          if(nums[i] < nums[i+1]) return false;
+        //     }
+        // }
+
+        // Approach 2 -->
+        boolean inc = true;
+        boolean dec = true;
+
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] < nums[i+1]){
+                dec = false;
             }
-        }
-        else{
-            for(int i = 0; i< nums.length-1; i++){
-                 if(nums[i] < nums[i+1]) return false;
+            else if(nums[i] > nums[i+1]){
+                inc = false;
             }
+
+            if(!dec && !inc) return false;
         }
 
         return true;
