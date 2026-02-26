c71b8270e6bb4a050da7fa87f7c3a10334047e13
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-02-26 22:16:11 +0530

    Time: 11 ms (70.16%), Space: 50.3 MB (56.97%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isTransferred(int arr[],int day,int m){
+        int capacity = 0;
+        int n = arr.length;
+        int d = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > m){
+                return false;
+            }
+            else if(capacity + arr[i] > m){
+                d++;
+                capacity = arr[i];
+                
+            }
+            else{
+                capacity += arr[i];
+            }
+        }
+
+        return d+1 <= day;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int high = 0;
+        int low = Integer.MAX_VALUE;
+        int ans = -1;
+        for(int i = 0; i < weights.length; i++){
+            low = Math.min(low,weights[i]);
+            high+=weights[i];
+        }
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isTransferred(weights,days,mid)){
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
