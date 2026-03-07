78bfe5f10fb7167322b88e49944403c4fa87247f
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-03-07 17:46:48 +0530

    Time: 15 ms (2.02%), Space: 52.7 MB (11.03%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,20 +3,25 @@ class Solution {
         List<String> ans = new ArrayList<>();
 
         for(int i = 1; i <= n; i++){
-            String str = "";
+            // String str = "";
 
-            if(i % 3 == 0){
-                str += "Fizz";
-            }
-            if(i % 5 == 0){
-                str += "Buzz";
-            }
-            if(i % 3 != 0 && i % 5 != 0){
-                str+=i;
-            }
-            ans.add(str);
+            // if(i % 3 == 0){
+            //     str += "Fizz";
+            // }
+            // if(i % 5 == 0){
+            //     str += "Buzz";
+            // }
+            // if(i % 3 != 0 && i % 5 != 0){
+            //     str+=i;
+            // }
+            // ans.add(str);
+
+            String str = i % 3 == 0 ? "Fizz" : "";
+            str += (i % 5 == 0 ? "Buzz" : "");
+
+            ans.add(str.equals("") ? str + i : str);
         }
 
-        return ans;
+        return  ans;
     }
 }
