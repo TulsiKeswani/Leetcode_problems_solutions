dade82470eb37e9c2fdcf4140291a41d22a5b545
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-03-13 15:30:03 +0530

    Time: 1 ms (73.42%), Space: 42.4 MB (99.51%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,24 +1,45 @@
 class Solution {
     public String interpret(String c) {
-       String s = "";
-        String ans = "";
-       for(int i = 0; i < c.length(); i++){
-            char ch = c.charAt(i);
-            s+=ch;
-            if(s.equals("G")){
-                ans+="G";
-                s = "";
-            }
-            else if(s.equals("()")){
-                ans+="o";
-                s = "";
-            }
-            else if(s.equals("(al)")){
-                ans+="al";
-                s = "";
-            }
-       }
+        // String s = "";
+        // String ans = "";
+        // for(int i = 0; i < c.length(); i++){
+        //     char ch = c.charAt(i);
+        //     s+=ch;
+        //     if(s.equals("G")){
+        //         ans+="G";
+        //         s = "";
+        //     }
+        //     else if(s.equals("()")){
+        //         ans+="o";
+        //         s = "";
+        //     }
+        //     else if(s.equals("(al)")){
+        //         ans+="al";
+        //         s = "";
+        //     }
+        // }
 
-       return ans;
+        // return ans;
+
+        StringBuilder sb = new StringBuilder("");
+        int i = 0;
+        while(i < c.length()){
+            char ch = c.charAt(i);
+
+            if(ch == 'G'){
+                sb.append(ch);
+                i++;
+            }
+            else if(c.charAt(i+1) == 'a'){
+                sb.append("al");
+                i+=4;
+            }
+            else{
+                sb.append("o");
+                i+=2;
+            }
+        }
+
+        return sb.toString();
     }
 }
