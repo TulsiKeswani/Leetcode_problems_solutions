8e0bd1d1b5169174060150f27d416463a0aa5a53
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-03-29 20:21:32 +0530

    Time: 3 ms (34.14%), Space: 48.8 MB (27.45%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,40 @@
 class Solution {
-    String map[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    static String map[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
-       ArrayList<String> ans = new ArrayList<>();
-       ans.add("");
-       for(int i = 0; i < digits.length(); i++){
-            int val = digits.charAt(i) - '0';
-            String str = map[val];
-            ArrayList<String> curr = new ArrayList<>();
+    //    ArrayList<String> ans = new ArrayList<>();
+    //    ans.add("");
+    //    for(int i = 0; i < digits.length(); i++){
+    //         int val = digits.charAt(i) - '0';
+    //         String str = map[val];
+    //         ArrayList<String> curr = new ArrayList<>();
+
+    //         for(int j = 0; j < str.length(); j++){
+    //             char ch = str.charAt(j);
+    //             for(String s : ans){
+    //                 curr.add(s + ch);
+    //             }
+    //         }
+
+    //         ans = curr;
+    //    }
+
+    //    return ans;
+
+        // Through Recursion
+        if(digits.length() == 0) {
+            List<String> l = new ArrayList<>(Arrays.asList(""));
+            return l;
+        }
+            List<String> smallAns = letterCombinations(digits.substring(1));
+            List<String> myAns = new ArrayList<>();
+            String str = map[digits.charAt(0) - '0'];
 
             for(int j = 0; j < str.length(); j++){
-                char ch = str.charAt(j);
-                for(String s : ans){
-                    curr.add(s + ch);
-                }
+               for(String s : smallAns){
+                    myAns.add(str.charAt(j) + s);
+               }
             }
 
-            ans = curr;
-       }
-
-       return ans;
+        return myAns;
     }
 }
