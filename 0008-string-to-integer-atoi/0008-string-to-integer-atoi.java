7bbe25be0f977d9585c027684cce9c0a48afa126
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-03 13:00:46 +0530

    Time: 2 ms (12.79%), Space: 43.5 MB (91.5%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,28 +1,56 @@
 class Solution {
     public int myAtoi(String s) {
-        long ans = 0;
-        byte isneg = 1;
-        s = s.trim();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(i == 0 && ch == '-'){
-                isneg = -1;
-                continue;
-            }
+        // long ans = 0;
+        // byte isneg = 1;
+        // s = s.trim();
+        // for(int i = 0; i < s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if(i == 0 && ch == '-'){
+        //         isneg = -1;
+        //         continue;
+        //     }
             
-            if(i == 0 && ch == '+') continue;
+        //     if(i == 0 && ch == '+') continue;
 
-            if(ans == 0 && ch == '0') continue;
+        //     if(ans == 0 && ch == '0') continue;
+
+        //     if(ch < '0' || ch > '9') break;
+
+        //     ans = ans * 10 + (ch - '0');
+        //     long curr = ans * isneg;
+        //     if(curr >= Integer.MAX_VALUE || curr <= Integer.MIN_VALUE){
+        //         return isneg < 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        //     }
+        // }
+       
+        // return (int)(ans * isneg);
+
+        // More cleaner way
+
+        s = s.trim();
+        if(s.length() == 0) return 0;
+        int i = 0;
+        boolean isneg = false;
+        if(s.charAt(i) == '-' || s.charAt(i) ==  '+'){
+            isneg = s.charAt(i) == '-';
+            i++;
+        }
+        long ans = 0;
+        while(i < s.length()){
+            char ch = s.charAt(i);
 
             if(ch < '0' || ch > '9') break;
 
             ans = ans * 10 + (ch - '0');
-            long curr = ans * isneg;
-            if(curr >= Integer.MAX_VALUE || curr <= Integer.MIN_VALUE){
-                return isneg < 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            if(isneg && (ans > Integer.MAX_VALUE)){
+                return Integer.MIN_VALUE;
             }
+            if(!isneg && (ans >= Integer.MAX_VALUE)){
+                return Integer.MAX_VALUE;
+            }
+            i++;
         }
-       
-        return (int)(ans * isneg);
+        int finalAns = (int)ans;
+        return isneg ? -finalAns : finalAns;
     }
 }
