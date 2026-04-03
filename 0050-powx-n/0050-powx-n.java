42fdf16f6b5b02ff07e8faa4ec002d482a3fda73
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-03 22:38:28 +0530

    Time: 0 ms (100%), Space: 48.2 MB (41.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,9 +1,10 @@
 class Solution {
     public double myPow(double x, int n) {
         if(n == 0) return 1;
-        double pow = myPow(x, Math.abs(n / 2));
-        double ans = n % 2 == 0? pow * pow : x * pow * pow;
-        
-        return n < 0 ? 1 / ans : ans;
+        double halfPow = myPow(x,Math.abs(n/2));
+        double fullPow = halfPow * halfPow;
+        fullPow = n % 2 == 0 ? fullPow : fullPow * x;
+
+        return n < 0 ? (1 / fullPow) : fullPow;
     }
 }
