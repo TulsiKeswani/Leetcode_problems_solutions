cc14dd364bb8ab7915be6a116fb42a6eac31c1c7
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-04 21:43:20 +0530

    Time: 8 ms (12.42%), Space: 47.7 MB (64.38%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -1,18 +1,36 @@
 class Solution {
     public List<String> validStrings(int n) {
-        if(n == 0) return new ArrayList<>(Arrays.asList(""));
+        // Using Recursion
 
-        List<String> smallAns = validStrings(n-1);
-        List<String> currAns = new ArrayList<>();
+        // if(n == 0) return new ArrayList<>(Arrays.asList(""));
 
-        for(String s : smallAns){ 
-            if(s.length() == 0 || s.charAt(0) != '0'){
-                currAns.add(0 + s);
+        // List<String> smallAns = validStrings(n-1);
+        // List<String> currAns = new ArrayList<>();
+
+        // for(String s : smallAns){ 
+        //     if(s.length() == 0 || s.charAt(0) != '0'){
+        //         currAns.add(0 + s);
+        //     }
+
+        //     currAns.add(1 + s);
+        // }
+
+        // return currAns;
+
+        // Using Loops
+        List<String> ans = new ArrayList<>();
+        ans.add("");
+        for(int i = 1; i <= n; i++){
+            List<String> temp = new ArrayList<>();
+            for(String s : ans){
+                if(s.length() == 0 || s.charAt(0) != '0'){
+                    temp.add(0 + s);
+                }
+                temp.add(1+s);
             }
-
-            currAns.add(1 + s);
+            ans = temp;
         }
 
-        return currAns;
+        return ans;
     }
 }
