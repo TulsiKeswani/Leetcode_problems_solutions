b4869f8ce05b4b865c9dd4cd3be64246ea8dac10
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-06 18:26:25 +0530

    Time: 0 ms (100%), Space: 44.2 MB (94.72%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,23 @@
 class Solution {
 
-    public List<String> generateParenthesisUtil(String s,int open,int close,List<String> ans){
-        if(close < open) return ans;
+    public List<String> generateParenthesisUtil(StringBuilder s,int open,int close,List<String> ans){
         if(close == 0 && open == 0){
-            ans.add(s);
+            ans.add(s.toString());
             return ans;
         }
-        if(open != 0) generateParenthesisUtil(s+"(",open-1,close,ans);
-        return generateParenthesisUtil(s+")",open,close-1,ans);
+        if(open != 0) {
+            generateParenthesisUtil(s.append("("),open-1,close,ans);
+            s.deleteCharAt(s.length()-1);
+        }
+        if(close != open){
+            generateParenthesisUtil(s.append(")"),open,close-1,ans);
+            s.deleteCharAt(s.length()-1);
+        }
+        return ans;
     }
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        generateParenthesisUtil("(",n-1,n,ans);
+        generateParenthesisUtil(new StringBuilder(""),n,n,ans);
 
         return ans;
     }
