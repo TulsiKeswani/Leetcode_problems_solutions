096d6960304a92bcf9b000adc8ad56026d2b03ca
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-21 19:47:41 +0530

    Time: 4 ms (50.46%), Space: 69.6 MB (65.16%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,19 +1,33 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int i = 0;
-        int j = 0;
+        // int i = 0;
+        // int j = 0;
+        // int sum = 0;
+        // double maxAvg = Integer.MIN_VALUE;
+
+        // while(j < nums.length){
+        //     sum += nums[j];
+
+        //     if(j - i + 1 == k ){
+        //         maxAvg = Math.max(maxAvg,(sum) / (k * 1.0));
+        //         sum-=nums[i];
+        //         i++;
+        //     }
+        //     j++;
+        // }
+
+        // return maxAvg;
         int sum = 0;
-        double maxAvg = Integer.MIN_VALUE;
+        for(int i = 0; i < k; i++){
+            sum+=nums[i];
+        }
 
-        while(j < nums.length){
-            sum += nums[j];
+        double maxAvg = sum * 1.0 / k;
 
-            if(j - i + 1 == k ){
-                maxAvg = Math.max(maxAvg,(sum) / (k * 1.0));
-                sum-=nums[i];
-                i++;
-            }
-            j++;
+        for(int i = k; i < nums.length; i++){
+            sum-=nums[i-k];
+            sum+=nums[i];
+            maxAvg = Math.max(maxAvg,sum * 1.0 / k);
         }
 
         return maxAvg;
