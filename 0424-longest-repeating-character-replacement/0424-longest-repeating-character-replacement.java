bf9f1b91c415e5096ffd08e31dbd506343632701
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-04-23 19:24:05 +0530

    Time: 6 ms (99.07%), Space: 46.1 MB (66.45%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,31 +1,39 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // brute force
-        int n = s.length();
-        int freq[] = new int[26];
+        
         int i = 0;
         int j = 0;
-        int ans = 0;
-        while(j < n){
-            char c = s.charAt(j);
-            freq[c - 'A']++;
-            int maxOccu = 0;
-            int count = 0;
-            for(int l = 0; l < freq.length; l++){
-                maxOccu = Math.max(maxOccu,freq[l]);
-                count+=freq[l];
-            }
-            int changeCount = (count - maxOccu);
-            if(changeCount <= k){
-                ans = Math.max(ans,j-i+1);
-            }
-            else{
-                freq[s.charAt(i) - 'A']--;
-                i++;
+        int maxfreq = 0;
+        int freq[] = new int[26];
+        int count = 0;
+        int maxLength = 0;
+        while(j < s.length()){
+            int ch = s.charAt(j) - 'A';
+            count++;
+
+            freq[ch]++;
+            if(freq[ch] > maxfreq){
+                maxfreq = freq[ch];
             }
 
+            int flips = count - maxfreq;
+
+            if(flips > k){
+                freq[s.charAt(i) - 'A']--;
+                count--;
+                int max = 0;
+                for(int l = 0; l < freq.length; l++){
+                    max = Math.max(max,freq[l]);
+                }
+                maxfreq = max;
+                i++;
+            }
+            else{
+                maxLength = Math.max(maxLength,j-i+1);
+            }
             j++;
         }
-        return ans;
+
+        return maxLength;
     }
 }
