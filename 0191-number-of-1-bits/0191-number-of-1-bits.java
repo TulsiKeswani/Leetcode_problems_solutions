b9c96a84261921fc173a8db593b28bb8be1d75fa
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-05-01 19:59:00 +0530

    Time: 1 ms (31.89%), Space: 42.3 MB (64.41%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int ans = 0;
+        while(n != 0){
+            int rem = n % 2;
+            ans += rem & 1;
+            n /= 2;
+        }
+
+        return ans;
+    }
+}
