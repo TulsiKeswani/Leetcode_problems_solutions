9609052164d207a2e167fbf9e7e20208073cfe13
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-20 09:54:07 +0530

    Time: 3 ms (34.29%), Space: 49.3 MB (7.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,40 +1,24 @@
 class Solution {
-    static String map[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    static String digit_map[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
     public List<String> letterCombinations(String digits) {
-    //    ArrayList<String> ans = new ArrayList<>();
-    //    ans.add("");
-    //    for(int i = 0; i < digits.length(); i++){
-    //         int val = digits.charAt(i) - '0';
-    //         String str = map[val];
-    //         ArrayList<String> curr = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        ans.add("");
+        for(int i = 0; i < digits.length(); i++){
+            List<String> curr = new ArrayList<>();
 
-    //         for(int j = 0; j < str.length(); j++){
-    //             char ch = str.charAt(j);
-    //             for(String s : ans){
-    //                 curr.add(s + ch);
-    //             }
-    //         }
+            int dig = digits.charAt(i) - '0';
 
-    //         ans = curr;
-    //    }
+            for(int j = 0; j < digit_map[dig].length(); j++){
 
-    //    return ans;
-
-        // Through Recursion
-        if(digits.length() == 0) {
-            List<String> l = new ArrayList<>(Arrays.asList(""));
-            return l;
-        }
-            List<String> smallAns = letterCombinations(digits.substring(1));
-            List<String> myAns = new ArrayList<>();
-            String str = map[digits.charAt(0) - '0'];
-
-            for(int j = 0; j < str.length(); j++){
-               for(String s : smallAns){
-                    myAns.add(str.charAt(j) + s);
-               }
+                for(String s : ans){
+                    curr.add(s + digit_map[dig].charAt(j));
+                }
             }
 
-        return myAns;
+            ans = curr;
+        }
+
+        return ans;
     }
 }
