ff0bdd1b8863668643c6092b5c1b458f1c67afe1
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-06-20 10:02:01 +0530

    Time: 3 ms (34.29%), Space: 48.6 MB (27.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,23 +2,44 @@ class Solution {
     static String digit_map[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
-        ans.add("");
-        for(int i = 0; i < digits.length(); i++){
-            List<String> curr = new ArrayList<>();
+        // Approach 1 -> With recursion
+        // base case
 
-            int dig = digits.charAt(i) - '0';
+        if(digits.length() == 0){
+            return new ArrayList<>(Arrays.asList(""));
+        }
+        List<String> smallAns = letterCombinations(digits.substring(1));
+        List<String> myAns = new ArrayList<>();
 
-            for(int j = 0; j < digit_map[dig].length(); j++){
+        String curr = digit_map[digits.charAt(0) - '0'];
 
-                for(String s : ans){
-                    curr.add(s + digit_map[dig].charAt(j));
-                }
+        for(int i = 0; i < curr.length(); i++){
+
+            for(String s : smallAns){
+                myAns.add(curr.charAt(i) + s);
             }
-
-            ans = curr;
         }
 
-        return ans;
+        return myAns;
+        // Approach 2 -> Without recursion;
+
+        // List<String> ans = new ArrayList<>();
+        // ans.add("");
+        // for(int i = 0; i < digits.length(); i++){
+        //     List<String> curr = new ArrayList<>();
+
+        //     int dig = digits.charAt(i) - '0';
+
+        //     for(int j = 0; j < digit_map[dig].length(); j++){
+
+        //         for(String s : ans){
+        //             curr.add(s + digit_map[dig].charAt(j));
+        //         }
+        //     }
+
+        //     ans = curr;
+        // }
+
+        // return ans;
     }
 }
