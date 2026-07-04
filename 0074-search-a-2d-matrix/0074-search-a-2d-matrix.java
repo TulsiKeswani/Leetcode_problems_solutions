5643019692c8e3e1e6e391e86d7294d5918ae4c2
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-04 21:21:14 +0530

    Time: 0 ms (100%), Space: 43.9 MB (61.77%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,25 +1,51 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        // Optimal approach --> 
 
-        int row = 0;
-        int col = matrix[0].length-1;
+        // Super Optimal Approach --> Olog(n * m)
 
-        while(row < matrix.length && col >= 0){
-            
+        int st = 0;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int end = n*m-1;
+
+        while(st <= end){
+            int mid = st + (end - st) / 2 ;
+
+            int row = mid / m;
+            int col = mid - (row * m);
+
             if(matrix[row][col] == target){
                 return true;
             }
             else if(matrix[row][col] < target){
-                row++;
+                st = mid+1;
             }
             else{
-                col--;
+                end = mid-1;
             }
         }
 
         return false;
+        // // Optimal approach --> 0(m + n)
+
+        // int row = 0;
+        // int col = matrix[0].length-1;
+
+        // while(row < matrix.length && col >= 0){
+            
+        //     if(matrix[row][col] == target){
+        //         return true;
+        //     }
+        //     else if(matrix[row][col] < target){
+        //         row++;
+        //     }
+        //     else{
+        //         col--;
+        //     }
+        // }
+
+        // return false;
 
         // // Brute force approach  --> O(n ^ 2)
 
