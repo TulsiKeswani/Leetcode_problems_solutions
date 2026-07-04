d42cb6bdc085f5498765fcf05fe6b94b8e3502be
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-04 20:43:59 +0530

    Time: 0 ms (100%), Space: 43.7 MB (90.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,15 +1,36 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Brute force approach  --> O(n ^ 2)
 
+        // Optimal approach --> 
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+        int row = 0;
+        int col = matrix[0].length-1;
 
-                if(matrix[i][j] == target) return true;
+        while(row < matrix.length && col >= 0){
+            
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
             }
         }
 
         return false;
+
+        // // Brute force approach  --> O(n ^ 2)
+
+
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[i].length; j++){
+
+        //         if(matrix[i][j] == target) return true;
+        //     }
+        // }
+
+        // return false;
     }
 }
