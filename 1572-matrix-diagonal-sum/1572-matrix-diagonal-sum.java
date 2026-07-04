ae34c208b7b81ae6bacf2235f5f0255da333c278
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-04 20:22:08 +0530

    Time: 0 ms (100%), Space: 46.2 MB (71.34%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,16 +1,31 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
+
+        // Optimal ---> O(n)
+
         int sum = 0;
-
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[i].length; j++){
-
-                if(i == j || (i + j) == mat.length-1){
-                    sum+=mat[i][j];
-                }
-            }
+        int n = mat.length - 1;
+        for(int i = 0; i <=n; i++){
+            sum+=mat[i][i];
+            sum+=mat[i][n-i];
         }
 
-        return sum;
+        return n % 2 != 0 ? sum : sum - mat[n/2][n/2];
+
+
+        // Brute force Approach (n ^ 2)
+        // int sum = 0;
+
+        // for(int i = 0; i < mat.length; i++){
+        //     for(int j = 0; j < mat[i].length; j++){
+
+        //         if(i == j || (i + j) == mat.length-1){
+        //             sum+=mat[i][j];
+        //         }
+        //     }
+        // }
+
+        // return sum;
+
     }
 }
