4156952e98eff57a0554e03fa7a245f870806714
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-06 10:42:57 +0530

    Time: 2 ms (99.34%), Space: 47.2 MB (39.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,19 +3,32 @@ class Solution {
         
         int ans[] = new int[2];
 
-        // Brute Force 
+        // Brute Force --> O(n^2)
 
-        for(int i = 0; i < nums.length-1; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    ans[0] = i;
-                    ans[1] = j;
+        // for(int i = 0; i < nums.length-1; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             ans[0] = i;
+        //             ans[1] = j;
 
-                    return ans;
-                }
+        //             return ans;
+        //         }
+        //     }
+        // }
+
+        // optimal approach --> O(n)
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int y = target - nums[i];
+            if(map.containsKey(y)){
+                return new int[]{map.get(y),i};
             }
-        }
 
-        return ans;
+            map.put(nums[i],i);
+        }
+        
+        return new int[2];
     }
 }
