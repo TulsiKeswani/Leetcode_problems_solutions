918b7b59af14ec1d2820c45dd5ab08a0f43495d1
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-15 23:55:08 +0530

    Time: 1 ms (100%), Space: 46.3 MB (58.25%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -3,22 +3,42 @@ class Solution {
         
         // Brute force Approach 
 
-        HashMap<Integer,Integer> freq = new HashMap<>();
+        // HashMap<Integer,Integer> freq = new HashMap<>();
 
+        // for(int i = 0; i < nums.length-1; i++){
+        //     if(nums[i] == key){
+        //         freq.put(nums[i+1],freq.getOrDefault(nums[i+1],0) + 1);
+        //     }
+        // }
+        // int ele = -1;
+        // int f = -1;
+        // for(Map.Entry<Integer,Integer> map : freq.entrySet()){
+        //     if(map.getValue() > f){
+        //         ele = map.getKey();
+        //         f = map.getValue();
+        //     }
+        // }
+
+        // return ele;
+
+        // optimal 
+
+        int freq[] = new int[1000];
         for(int i = 0; i < nums.length-1; i++){
             if(nums[i] == key){
-                freq.put(nums[i+1],freq.getOrDefault(nums[i+1],0) + 1);
-            }
-        }
-        int ele = -1;
-        int f = -1;
-        for(Map.Entry<Integer,Integer> map : freq.entrySet()){
-            if(map.getValue() > f){
-                ele = map.getKey();
-                f = map.getValue();
+                freq[nums[i+1] - 1]++;
             }
         }
 
-        return ele;
+        int maxF = 0;
+        int ans = -1;
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] > maxF){
+                maxF = freq[i];
+                ans = i+1;
+            }
+        }
+
+        return ans;
     }
 }
