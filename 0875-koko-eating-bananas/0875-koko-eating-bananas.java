dc2126427ff6da5d1d9ecc2821785eebffbed49b
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-09-18 10:43:29 +0530

    Time: 25 ms (18.68%), Space: 47.9 MB (42.67%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,30 +1,32 @@
 class Solution {
-    public boolean canEat(int arr[],int k,int h){
-        int time = 0;
+    public boolean isPossible(int arr[],int k,int h){
+        int req = 0;
         for(int i = 0; i < arr.length; i++){
-            double div = arr[i] * 1.0 / k;
-            time+=Math.ceil(div);
+            req += Math.ceil((arr[i] * 1.0) / k);
         }
-        return !(time > h);
+
+        return req <= h;
     }
     public int minEatingSpeed(int[] piles, int h) {
-            int min = 1;
-            int max = Integer.MIN_VALUE;
-            int ans = max;
-            for(int i = 0;i < piles.length; i++){
-                max = Math.max(max,piles[i]);
-            }
-            while(min <= max){
-                int mid = min + (max - min) / 2;
-                if(canEat(piles,mid,h)){
-                    ans = mid;
-                    max = mid-1;
-                }
-                else{
-                    min = mid+1;
-                }
-            }
+        int min = 1;
+        int max = Integer.MIN_VALUE;
+        int ans = -1;
+        for(int i = 0; i < piles.length; i++){
+            max = Math.max(max,piles[i]);
+        }
 
-            return ans;
+        while(min <= max){
+            int mid = min + (max - min) /2;
+
+            if(isPossible(piles,mid,h)){
+                ans = mid;
+                max = mid-1;
+            }
+            else{
+                min = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
